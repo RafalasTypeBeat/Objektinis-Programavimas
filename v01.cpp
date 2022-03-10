967cc158250b420328e8994490b157156b5e3ec8
Rapolas Kairys <rapolas.kairys@gmail.com>
2022-03-10 15:58:35 +0200

    pirma dalis. Ivedimas i struktura ir isvedimas.

--- v01.cpp
+++ v01.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::setw;
+
+struct mokinys
+{
+    string name = "", surename = "";
+    int hw, egzam;
+    float rez;
+};
 
 int main()
 {
-    int n;
-    std::cin>>n;
-    std::cin>>n;
-    std::cout<<"hello";
+    mokinys mokinys1;
+    cout << "Iveskite mokinio varda: ";
+    cin >> mokinys1.name;
+    cout << "Iveskite mokinio pavarde: ";
+    cin >> mokinys1.surename;
+    cout << "Iveskite namu darbo rezultata: ";
+    cin >> mokinys1.hw;
+    cout << "Iveskite egzamino rezultata: ";
+    cin >> mokinys1.egzam;
+    cout << setw(15) << mokinys1.name << setw(15) << mokinys1.surename << setw(20) << "Galutinis (Vid.)";
+    cout << "-----------------------------------"
 }
