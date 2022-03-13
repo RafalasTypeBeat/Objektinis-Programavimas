12b09c2b6cb78fdbcab82abd92ed793aa7fdd9e4
Rapolas Kairys <rapolas.kairys@gmail.com>
2022-03-13 14:54:41 +0200

    Prideta funkcija skaiciuoti mediana

--- v01.cpp
+++ v01.cpp
@@ -11,23 +11,45 @@ using std::endl;
 struct mokinys
 {
     string name = "", surename = "";
-    int hw, egzam;
-    double rez;
+    int hw[3], egzam;
+    double rez = 0;
+    int mediana;
 };
 
 int main()
 {
+    char out;
     mokinys mokinys1;
     cout << "Iveskite mokinio varda: ";
     cin >> mokinys1.name;
     cout << "Iveskite mokinio pavarde: ";
     cin >> mokinys1.surename;
-    cout << "Iveskite namu darbo rezultata: ";
-    cin >> mokinys1.hw;
+    
+    
+    
+    cout << "Iveskite namu darbo rezultata: " << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> mokinys1.hw[i];
+        mokinys1.rez += mokinys1.hw[i];
+    }
     cout << "Iveskite egzamino rezultata: ";
     cin >> mokinys1.egzam;
-    cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(20) << "Galutinis (Vid.)"<<endl;
-    cout << "--------------------------------------------------"<<endl;
-    mokinys1.rez = ((float)(mokinys1.hw + mokinys1.egzam)) / 2;
-    cout << setw(15) << mokinys1.name << setw(15) << mokinys1.surename <<setw(7) << std::setprecision(3) << mokinys1.rez;
+    cout << "Isvesti vidurki ar mediana? V/M ";
+    while (out != 'V' && out != 'M') cin >> out;
+    cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(39) << "Galutinis (Vid.) / Galutinis (Med.)" <<endl;
+    cout << "---------------------------------------------------------------------"<<endl;
+
+    
+    cout << setw(15) << mokinys1.name << setw(15) << mokinys1.surename;
+    if(out == 'V') 
+    {
+        mokinys1.rez = (mokinys1.rez + mokinys1.egzam) / 4;
+        cout << setw(7) << std::setprecision(3) << mokinys1.rez;
+    }
+    else
+    {
+        cout << setw(26) << std::setprecision(3) << (float(mokinys1.hw[1] + mokinys1.hw[2]))/2;
+    }
+    
 }
