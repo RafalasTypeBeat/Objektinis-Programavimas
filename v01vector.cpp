07aaa609a88b43764d83eb4d87292c2264b295e9
Rapolas Kairys <rapolas.kairys@gmail.com>
2022-03-20 22:45:18 +0200

    Kodas sudetas i funkcijas, pridetas dinamiskumas

--- v01vector.cpp
+++ v01vector.cpp
@@ -15,79 +15,102 @@ struct mokinys
     string name = "", surename = "";
     std::vector<int> hw;
     int egzam;
-    double rez = 0;
-    int mediana;
+    int size = 0;
+    double mediana;
 };
-
+int randP();
+void input(std::vector<mokinys>& mok, int &i);
+void print(std::vector<mokinys> mok, int i);
 
 int main()
+{
+    std::vector<mokinys> mok;
+    int i;
+    input(mok, i);
+    print(mok, i);
+}
+
+void input(std::vector<mokinys>& mok, int &i)
+{ 
+    char outhw, outhwrand, outegzrand, outaddmok;
+    i = 0;
+    int nd, ndsize;
+    while (outaddmok != 'n')
+    {
+        ndsize = 0;
+        cout << "Iveskite mokinio varda: ";
+        mok.push_back(mokinys());
+        cin >> mok[i].name;
+        cout << "Iveskite mokinio pavarde: ";
+        cin >> mok[i].surename;
+        outhwrand = '0';
+        cout << "Ar norite automatiskai generuoti namu darbu rezultatus?(t/n) ";
+        while (outhwrand != 't' && outhwrand != 'n') cin >> outhwrand;
+        if (outhwrand == 't')
+        {
+            mok[i].size = 5;
+            for (int g = 0; g < 5; g++)
+            {
+                mok[i].hw.push_back(randP());
+            }
+        }
+        else
+        {
+            cout << "Iveskite pazymius: ";
+            while(cin >> nd)
+            {
+                mok[i].hw.push_back(nd);
+                ndsize++;
+            }
+            mok[i].size = ndsize;
+            std::cin.clear();
+            //std::cin.ignore(INT_MAX);
+        }
+        outegzrand = '0';
+        cout << "Ar norite automatiskai generuoti egzmino rezultata?(t/n)";
+        while (outegzrand != 't' && outegzrand != 'n') cin >> outegzrand;
+        if (outegzrand == 't') mok[i].egzam = randP();
+
+        else
+        {
+            cout << "Iveskite egzamino rezultata: ";
+            cin >> mok[i].egzam;
+        }
+
+        outaddmok = '0';
+        cout << "Ar norite prideti dar viena mokini?(t/n)? ";
+        while (outaddmok != 't' && outaddmok != 'n') cin >> outaddmok;
+        if(outaddmok == 't')i++;
+    }
+}
+
+void print(std::vector<mokinys> mok, int i)
+{
+    cout << "Pavarde" << setw(15) << "Vardas" << setw(39) << "Galutinis (Vid.) / Galutinis (Med.)" <<endl;
+    cout << "---------------------------------------------------------------------"<<endl;
+    double temp;
+    int vid;
+    for (int j = 0; j <= i; j++)
+    {
+        cout <<  mok[j].surename << setw(17) << mok[j].name;
+        temp = 0;
+
+        for (int g = 0; g < mok[j].size; g++)
+        {
+            temp += mok[j].hw[g];
+        }
+        cout << setw(13) << std::fixed << std::setprecision(2) << (0.6 * mok[j].egzam + ((0.4 * temp) / mok[j].size));
+        vid = mok[j].size / 2;
+        mok[j].mediana = mok[j].size % 2 == 0 ? (mok[j].hw[vid - 1] + mok[j].hw[vid]) / 2
+                                              : mok[j].hw[vid];
+        cout << setw(18) << std::fixed << std::setprecision(2) << mok[j].mediana << endl;
+    }
+}
+
+int randP()
 {
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_int_distribution<int> dist(1, 10);
-    char out;
-    mokinys mokinys1;
-    cout << "Iveskite mokinio varda: ";
-    cin >> mokinys1.name;
-    cout << "Iveskite mokinio pavarde: ";
-    cin >> mokinys1.surename;
-    
-    
-    int temp, i = 0, size;
-    cout << "Ar norite automatiskai generuoti namu darbu rezultatus?(T/N) ";
-    while (out != 'T' && out != 'N') cin >> out;
-    cout << "Kiek yra namu darbu rezultatu? "; cin>>size;
-    if (out == 'T') for (i = 0; i < size; i++)
-    {
-        mokinys1.hw.push_back(dist(mt));
-    }
-    else{
-    cout << "Iveskite namu darbu rezultatatus: " << endl;
-    for(i = 0; i < size; i++)
-    {
-        cin >> temp;
-        if (temp > 0 && temp <= 10)
-        {
-            mokinys1.hw.push_back(temp);
-        }
-    }
-    }
-    out = '0';
-    cout << "Ar norite automatiskai generuoti egzamino rezultata?(T/N) ";
-    while (out != 'T' && out != 'N') cin >> out;
-    if (out == 'T') 
-    {
-        mokinys1.egzam = dist(mt);
-    }
-    else{
-    cout << "Iveskite egzamino rezultata: ";
-    cin >> mokinys1.egzam;
-    }
-    cout << "Isvesti vidurki ar mediana? (V/M) ";
-    while (out != 'V' && out != 'M') cin >> out;
-    cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(39) << "Galutinis (Vid.) / Galutinis (Med.)" <<endl;
-    cout << "---------------------------------------------------------------------"<<endl;
-    cout << setw(15) << mokinys1.name << setw(15) << mokinys1.surename;
-    temp = 0;
-    if(out == 'V') 
-    {
-        if(size == 0) cout << mokinys1.egzam;
-        else{
-            for (auto& n : mokinys1.hw)
-                temp += n;
-        cout<<temp;
-        mokinys1.rez = (0.6 * mokinys1.egzam + 0.4 * temp) / size;
-        cout << setw(8) << std::setprecision(3) << mokinys1.rez;
-        }
-    }
-    else
-    {
-        if(size % 2 == 0)
-        cout << setw(26) << std::setprecision(3) <<  mokinys1.hw.at(size / 2);
-        else if (size == 0) cout << setw(26) << mokinys1.egzam;
-        else if (size == 1) cout << setw(26) << std::setprecision(3) << (float(mokinys1.hw.at(0) + mokinys1.egzam))/2;
-        else
-        cout << setw(26) << std::setprecision(3) << (float(mokinys1.hw.at((size + 1) / 2) + mokinys1.hw.at((size - 1) / 2)) / 2);
-    }
-    
+    return dist(mt);
 }
